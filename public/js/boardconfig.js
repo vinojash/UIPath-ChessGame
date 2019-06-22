830ac16bf5d6f716aec521265f13540dedc48b93
DESKTOP-CC125MC\VinoJash <vinojash@live.com>
2019-06-22 12:12:17 +0400

    initial commit

---
+++ public/js/boardconfig.js
@@ -0,0 +1,63 @@
+var board,
+  game = new Chess();
+
+// Actions after any move
+var onMoveEnd = function(oldPos, newPos) {
+  // Alert if game is over
+  if (game.game_over() === true) {
+    alert("Game Over");
+    console.log("Game Over");
+  }
+
+  // Log the current game position
+  console.log(game.fen());
+};
+
+// Check before pick pieces that it is white and game is not over
+var onDragStart = function(source, piece, position, orientation) {
+  // if (game.game_over() === true || piece.search(/^b/) !== -1) {
+  //   return false;
+  // }
+};
+
+// Update the board position after the piece snap
+// for castling, en passant, pawn promotion
+var onSnapEnd = function() {
+  board.position(game.fen());
+};
+
+// Handles what to do after human makes move.
+// Computer automatically makes next move
+var onDrop = function(source, target) {
+  // see if the move is legal
+  var move = game.move({
+    from: source,
+    to: target,
+    promotion: "q" // NOTE: always promote to a queen for example simplicity
+  });
+
+  // If illegal move, snapback
+  if (move === null) return "snapback";
+
+  // Log the move
+  console.log(move);
+  /*
+  // make move for black
+  window.setTimeout(function() {
+    makeMove(4, 3);
+  }, 250);
+  */
+};
+
+// Configure board
+var cfg = {
+  draggable: true,
+  position: "start",
+  // Handlers for user actions
+  onMoveEnd: onMoveEnd,
+  onDragStart: onDragStart,
+  onDrop: onDrop,
+  onSnapEnd: onSnapEnd
+};
+board = ChessBoard("board", cfg);
+
